1209679d255591020b76b0f5ddba2a2c871d9337
Darius Saunders <dariussaunders24@gmail.com>
2026-05-27 21:02:52 -0400

    Updated vendors page

--- app/vendors/page.tsx
+++ app/vendors/page.tsx
@@ -74,7 +74,7 @@ export default function VendorsPage() {
           automotive partners. Click any vendor name below to visit their website.
         </p>
 
-        <p className="mt-4 text-sm text-gray-400">
+        <p className="mt-4 text-md text-gray-400">
           For discount codes and details, email{" "}
           <a
             href="mailto:dariussaunders24@gmail.com"
